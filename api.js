eadcdb278d8a6896b4b01a8b7e0f52025674f22f
neerajp926 <neerajp926@gmail.com>
2022-03-03 22:02:25 -0700

    Delete by ID and Update by params Method

--- api.js
+++ api.js
@@ -39,16 +39,115 @@ async function getStudentById(search_id){
     )
 }
 
+async function deleteStudentByID(student_id){
+    // var result = getStudentById(student_id).then(res => {
+    //     console.log(res)
+    //     console.log("Result found")}).catch(err => console.log(err))
+   // console.log(result)
+    sql.connect(dbConn)
+    .then(
+        sql => {
+            sql
+            .request()
+            .query(`Delete from student where id=${student_id}`)
+            .then(
+                resp => {
+                    // query successful
+                    console.info(resp.recordsets[0])
+
+                }
+            )
+            .catch(
+                err => console.error("Query not execute \n" + err)
+            )
+        }
+    )
+    .catch(
+        err => {
+            console.error('Connection NOT Established \n' + err)
+        }
+    )
+}
+
+//insertStudent
+async function insertStudent(...params){
+    sql.connect(dbConn)
+    .then(
+        sql => {
+            sql
+            .request()
+            .query(`Insert into student ([first_name]
+                ,[middle_name]
+                ,[last_name]
+                ,[date_of_birth]
+                ,[gender], [date_created]
+                ,[date_modified]
+                ,[is_deleted]) values ('${params[0]}','${params[1]}','${params[2]}',
+                 PARSE('${params[3]}' as date using 'en'),'${params[4]}',GETDATE(), GETDATE(), 0)`)
+            .then(
+                resp => {
+                    // query successful
+                    console.info(resp.recordsets[0])
+
+                }
+            )
+            .catch(
+                err => console.error("Query not execute \n" + err)
+            )
+        }
+    )
+    .catch(
+        err => {
+            console.error('Connection NOT Established \n' + err)
+        }
+    )
+}
+
+async function updateStudent(...params){
+    sql.connect(dbConn)
+    .then(
+        sql => {
+            sql
+            .request()
+            .query(`Update student Set [first_name] = '${params[1]}',[middle_name] = '${params[2]}'
+            ,[last_name] = '${params[3]}'
+            ,[date_of_birth] = PARSE('${params[4]}' as date using 'en')
+            ,[gender] = '${params[5]}' where [id] = ${params[0]}`)
+            .then(
+                resp => {
+                    // query successful
+                    console.info(resp.recordsets[0])
+
+                }
+            )
+            .catch(
+                err => console.error("Query not execute \n" + err)
+            )
+        }
+    )
+    .catch(
+        err => {
+            console.error('Connection NOT Established \n' + err)
+        }
+    )
+}
 // student
 // read students    all , by id
 // delete students  all,  by id
 // insert students  by 1 row
 // update students  by 1 row
 
-getStudents()           // fetch list of all students
-getStudentById(1)       // fetch the student whose id is 1
- 
+//getStudents()           // fetch list of all students
+//pass ID to get the result
+//getStudentById(1)       // fetch the student whose id is 1
 
+deleteStudentByID(17);
+//deleteStudentByID(16);
+//Pass parameter as first_name, middle_name, last_name, date_of_birth, gender
+//insertStudent('Kim','','Pantor','01/01/2020', 'M')
+
+//pass student ID, and other parameter to update
+//updateStudent(2,'Neearj','','Patel','01/01/1998',"M")
 
 /**
  * Tables -> student, grade, course, student grade
